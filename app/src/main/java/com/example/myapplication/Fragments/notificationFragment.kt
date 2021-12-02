703995ce1e210c968fd35700c7a0f66b6925489b
davitbediashvili <davit.bediashvili.1@btu.edu.ge>
2021-12-02 19:48:43 +0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Fragments/notificationFragment.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.Fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class notificationFragment: Fragment(R.layout.fragment_notification) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
